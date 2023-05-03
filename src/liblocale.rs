f7e60b56cf866f65faeaaf66bc9e2f3471a06341
Kit <kit@FossilBookAir.local>
2023-05-03 15:08:45 +1200

    edit liblocale for new cfgdir

--- src/liblocale.rs
+++ src/liblocale.rs
@@ -6,8 +6,8 @@ pub fn langstr(app: &str, placeholder: &str) -> String {
     let locale = whoami::lang().collect::<Vec<String>>();
     let lang = locale[1].clone();
     let home = get_home();
-    let langpath = format!("{home}/swaycfg/locale/{app}/{lang}.toml");
-    let backup_path = format!("{home}/swaycfg/locale/{app}");
+    let langpath = format!("{home}/Oceania/locale/{app}/{lang}.toml");
+    let backup_path = format!("{home}/Oceania/locale/{app}");
     let langfile = match read_to_string(langpath.clone()) {
         Ok(x) => x,
         Err(..) => {
